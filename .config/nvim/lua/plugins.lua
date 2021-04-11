2b2275d8dda35909071cd6afc2e68acabb855db8
equk <equk@users.noreply.github.com>
2021-04-11 16:23:44 +0100

    added plugins management config

---
+++ .config/nvim/lua/plugins.lua
@@ -0,0 +1,28 @@
+-- init plugin manager
+return require('packer').startup(function(use)
+    -- packer can manage itself
+    use 'wbthomason/packer.nvim'
+    -- load plugins
+    use 'airblade/vim-gitgutter'
+    use 'anott03/nvim-lspinstall'
+    use 'honza/vim-snippets'
+    use 'itchyny/lightline.vim'
+    use 'itchyny/vim-gitbranch'
+    use 'jiangmiao/auto-pairs'
+    use 'jremmen/vim-ripgrep'
+    use 'neovim/nvim-lspconfig'
+    use 'nvim-lua/completion-nvim'
+    use 'nvim-lua/plenary.nvim'
+    use 'nvim-lua/popup.nvim'
+    use 'nvim-lua/telescope.nvim'
+    use 'nvim-treesitter/nvim-treesitter'
+    use 'sirver/ultisnips'
+    use 'tpope/vim-commentary'
+    use 'tpope/vim-surround'
+    use 'tjdevries/colorbuddy.nvim'
+    use 'tjdevries/gruvbuddy.nvim'
+    use {
+        'lukas-reineke/indent-blankline.nvim',
+        branch = 'lua'
+    }
+end)
