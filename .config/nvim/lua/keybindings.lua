965cbc4e45973637aee463048d5146fa9d6cbbb2
equk <equk@users.noreply.github.com>
2021-09-03 12:47:04 +0100

    added keybindings for moving lines up/down

--- .config/nvim/lua/keybindings.lua
+++ .config/nvim/lua/keybindings.lua
@@ -13,3 +13,9 @@ key_mapper('n', '<leader>fh', ':lua require"telescope.builtin".help_tags()<CR>')
 key_mapper('n', '<leader>fb', ':lua require"telescope.builtin".buffers()<CR>')
 key_mapper('n', '<leader>gf', ':lua require"telescope.builtin".git_files()<CR>')
 key_mapper('n', '<leader>gs', ':lua require"telescope.builtin".git_status()<CR>')
+
+-- move lines
+key_mapper('n', '<A-up>', ':m .-2<CR>')
+key_mapper('n', '<A-down>', ':m .+1<CR>')
+key_mapper('n', '<A-k>', ':m .-2<CR>')
+key_mapper('n', '<A-j>', ':m .+1<CR>')
